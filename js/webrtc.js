760ff245d186fb81e1e0907dc64c99094417e3b4
Tsukasa Horinouchi <tsukasa.horinouchi@groove-x.com>
2020-07-31 12:52:35 +0900

    refactor

--- js/webrtc.js
+++ js/webrtc.js
@@ -13,67 +13,64 @@ const waitForButtonClick = button => {
 }
 
 //--------------------------------------------------
-// webRTC
+// WebRTC
 //--------------------------------------------------
 async function startSession(pc) {
-    let session_id = document.getElementById("session_id").value;
-    // サーバが投げてくるofferを受け取る
-    fetch(`https://webrtc-sdp-exchanger.appspot.com/sessions/${session_id}/offer`, {
-        method: "GET",
-        mode: "cors",
-        headers: new Headers({"Content-Type": "application/json; charset=utf-8"}),
-    }).then((response) => {
-        console.log("step2");
-        console.log(response);
-        if (response.ok) {
-            return response.json();
-        }
-        return Promise.reject(new Error("invalid response"));
-    }).then((json) => {
-        console.log("catch session desc");
-        try {
-            console.log(json.session_description)
-            pc.setRemoteDescription(new RTCSessionDescription(json.session_description));
-            pc.createAnswer().then((d) => {
-                pc.setLocalDescription(d)
-                fetch(`https://webrtc-sdp-exchanger.appspot.com/sessions/${session_id}`, {
-                    method: "POST",
-                    mode: "cors",
-                    headers: new Headers({"Content-Type": "application/json; charset=utf-8"}),
-                    body: JSON.stringify({session_description: d, session_id: session_id}),
-                }).then((response) => {
-                    console.log("step3");
-                    console.log(response);
-                }).catch(err => console.error);
-            }).catch(log);
-        } catch (e) {
-            alert(e);
-        }
-        document.getElementById('startSession').disabled = "true";
-    }).catch(err => console.error);
-};
+    const session_id = document.getElementById("session_id").value;
+    
+    try {
+        // サーバが投げてくるofferを受け取る
+        response = await fetch(`https://webrtc-sdp-exchanger.appspot.com/sessions/${session_id}/offer`, {
+            method: "GET",
+            mode: "cors",
+            headers: new Headers({"Content-Type": "application/json; charset=utf-8"}),
+        }).then((response) => {
+            return response.json()
+        })
+    
+        console.log(response.session_description)
 
+        // リモートとローカルのSDPを設定
+        await pc.setRemoteDescription(new RTCSessionDescription(response.session_description))
+        answer = await pc.createAnswer()
+        await pc.setLocalDescription(answer)
+
+        // answerを相手に送る
+        await fetch(`https://webrtc-sdp-exchanger.appspot.com/sessions/${session_id}`, {
+            method: "POST",
+            mode: "cors",
+            headers: new Headers({"Content-Type": "application/json; charset=utf-8"}),
+            body: JSON.stringify({session_description: answer, session_id: session_id}),
+        })
+        console.log("success to start session")
+    } catch(error) {
+        throw new Error("start session error")
+    }
+};
 
 //--------------------------------------------------
 // main
 //--------------------------------------------------
-let log = msg => {
-    document.getElementById('div').innerHTML += msg + '<br>'
-};
-
 async function main() {
+    let log = msg => {
+        document.getElementById('div').innerHTML += msg + '<br>'
+    };
+    
     let pc = new RTCPeerConnection({
         iceServers: [{urls: 'stun:stun.l.google.com:19302'}]
     });
     
+    // videoの準備
     const video = document.getElementById('localVideo');
     stream = await navigator.mediaDevices.getUserMedia({video: true, audio: false})
     video.srcObject = stream 
 
+    // videoをstreamに設定
     for (const track of stream.getTracks()) {
         pc.addTrack(track, stream);
     }
 
+    // data channelの受信コールバックの設定
     pc.ondatachannel = event => {
         const dc = event.channel
         dc.onmessage = ev => {
@@ -81,11 +78,14 @@ async function main() {
         }
     }
 
+    // startSessionボタンが押されたらsession開始
     const startButton = document.getElementById("startSession")
     await waitForButtonClick(startButton)
-    console.log("clicked!!")
-    await startSession(pc)
-
+    try {
+        await startSession(pc)
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 main();
